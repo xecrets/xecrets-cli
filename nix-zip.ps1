c28da3aff5db88da64aed2fec78378fe560169c8
Svante Seleborg <svante@axantum.com>
2025-09-16 10:26:16 +0200

    :alembic: Investigate wsl: Failed to start the systemd user session #129

---
+++ nix-zip.ps1
@@ -0,0 +1,29 @@
+﻿Param (
+    [Parameter(Mandatory=$true)][string]$Version,
+    [Parameter(Mandatory=$true)][string]$Directory,
+    [Parameter(Mandatory=$true)][string]$Platform
+)
+
+$ErrorActionPreference = 'Stop'
+Push-Location -Path "src\Xecrets.Cli\bin\Release\net8.0\publish\$Directory"
+try {
+    $cmd = @(
+        "exec {BASH_XTRACEFD}>&1"
+        "set -euox pipefail"
+        "chmod 775 XecretsCli"
+        "mkdir 'XecretsCli-$Platform-$Version'"
+        "cp -r XecretsCli 'XecretsCli-$Platform-$Version'"
+        "tar -czvf 'XecretsCli-$Platform-$Version.tar.gz' 'XecretsCli-$Platform-$Version'"
+        "rm -rf 'XecretsCli-$Platform-$Version'"
+    ) -join " && "
+    
+    wsl bash -lc "$cmd"
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "WSL command failed with exit code $LASTEXITCODE"
+    }
+}
+finally
+{
+    Pop-Location
+}
---
+++ win-zip.ps1
@@ -0,0 +1,19 @@
+﻿Param (
+    [Parameter(Mandatory=$true)][string]$Version,
+    [Parameter(Mandatory=$true)][string]$Directory,
+    [Parameter(Mandatory=$true)][string]$Platform
+)
+
+$workdir = (Get-Location)
+Push-Location -Path "src\Xecrets.Cli\bin\Release\net8.0\publish\$Directory"
+try {
+    $compress = @{
+        Path = "XecretsCli.exe"
+        DestinationPath = "XecretsCli-$Platform-$Version.zip"
+    }
+    Compress-Archive @compress
+}
+finally
+{
+    Pop-Location
+}
